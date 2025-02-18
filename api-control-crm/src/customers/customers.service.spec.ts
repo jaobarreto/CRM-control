c6ddab0a41680fa67728b7ef3f8e8e4ac6075767
João Barreto <joaobarretoprof@gmail.com>
2025-02-18 11:44:12 -0300

    test: add CRUD tests for users

--- api-control-crm/src/customers/customers.service.spec.ts
+++ api-control-crm/src/customers/customers.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersService } from './customers.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('CustomersService', () => {
   let service: CustomersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomersService],
+      providers: [CustomersService, PrismaService],
     }).compile();
 
     service = module.get<CustomersService>(CustomersService);
